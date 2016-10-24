17b444fd5854d2ee0e418c035d3e5bc8cdb46747
graholm <graholm@nuevaschool.org>
2016-10-24 09:05:18 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ var nameInput = $("#name-input")
 var noteInput = $("#note-input")
 var noteList = $("#note-list")
 
+nameInput.keydown(function(e) {
+  if (e.which == 13) {
+    var valueToSave = nameInput.val();
+    addListItem("nameList", valueToSave);
+    console.log (valueToSave)
+  }	
+})
+
 function getNotesForName(name) {  
-  
-  noteInput.keydown(function(e) {
-    if (e.which == 13) {
-      var valueToSave = nameInput.val();
-      addListItem("nameList", valueToSave);
-      console.log (valueToSave)
-    }	
-  })
   if (e.which == 13){
     var valueToSave = noteInput.val();
-    console.log (valueToSave)
   } 	
   onNewListItem(name + "/notes", function(note) )
     console.log(note)
