1d72ec9fcba05c34f642fd4f6997a031de401d82
graholm <graholm@nuevaschool.org>
2016-10-24 08:52:56 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-addListItem( (name) + "/notes", (note) )
+addListItem( (nameInput) + "/notes", (noteList) )
 
-var name;
 var nameInput = $("#name-input")
-
 var noteInput = $("#note-input")
 var noteList = $("#note-list")
 
 nameInput.keydown(function(e) {
   if (e.which == 13) {
-    // we have a new name! Now what?
   }	
 })
 
